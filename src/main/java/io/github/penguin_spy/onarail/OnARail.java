030b4ed8f38c38e58319591c948a24765085617b
Penguin_Spy <penguinspy2435@outlook.com>
2022-06-16 20:23:07 -0700

    Configure project

--- src/main/java/io/github/penguin_spy/onarail/OnARail.java
+++ src/main/java/io/github/penguin_spy/onarail/OnARail.java
@@ -1,15 +1,15 @@
-package com.example.example_mod;
+package io.github.penguin_spy.onarail;
 
 import org.quiltmc.loader.api.ModContainer;
 import org.quiltmc.qsl.base.api.entrypoint.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class ExampleMod implements ModInitializer {
+public class OnARail implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod name as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final Logger LOGGER = LoggerFactory.getLogger("Example Mod");
+	public static final Logger LOGGER = LoggerFactory.getLogger("On A Rail");
 
 	@Override
 	public void onInitialize(ModContainer mod) {
